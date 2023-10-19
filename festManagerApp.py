1ba10d4756cfe77fcb43b8576e688043e8a3d41b
Aaron Santiago <santiaaa@shernet.sheridancollege.ca>
2023-10-19 16:25:07 -0400

    Project Setup Complete.

---
+++ festManagerApp.py
@@ -0,0 +1,14 @@
+"""The entry point of the application. Implements UI related code"""
+
+class FestManagerApp:
+    
+    # PI create method main with the functionality described in the print document
+    def main():
+        print("Food Fest Manager - Aaron Santiago")
+
+    main()
+    # PIII write the code to implement the User Interaction as described in print document
+    pass
+
+# PI create instance of FestManagerApp
+# PI call the method main using the instance of FestManagerApp
---
+++ foodFest.py
@@ -0,0 +1,12 @@
+"""Implements the logic associated with a food fest"""
+
+
+# PI create a class FoodFest
+class FoodFest:
+
+
+# PII create initailizer of FoodFest as per the instructions in print document
+# PII define accessors and mutators as per the instructions in print document
+# PII define method computeNetRegularCost as per the instructions in print document
+# PII define method computeNetPremiumCost as per the instructions in print document
+# PII define method requestReservation as per the instructions in print document
