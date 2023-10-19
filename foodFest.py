cd1f712d2e6ec01fe9443deca6a549ce312c99db
Aaron Santiago <santiaaa@shernet.sheridancollege.ca>
2023-10-19 16:32:50 -0400

    Food fest constructor created.

--- foodFest.py
+++ foodFest.py
@@ -3,6 +3,20 @@
 
 # PI create a class FoodFest
 class FoodFest:
+    
+    def __init__(self, festName, regStallCount, premStallCount, revCost, tax, regRevCount, premRevCount):
+        self.festName = festName
+        self.regStallCount = regStallCount
+        self.premStallCount = premStallCount
+        self.revCost = revCost
+        self.tax = tax
+        self.regRevCount = regRevCount
+        self.premRevCount = premRevCount
+        regRevCount = 0
+        premRevCount = 0
+
+    
+        
 
 
 # PII create initailizer of FoodFest as per the instructions in print document
